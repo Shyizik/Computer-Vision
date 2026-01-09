24f60c423cbaf0b0f4cb37746b49a9683475cfaa
Shyizik <you@example.com>
2026-01-09 15:16:03 +0200

    upload

--- Lab_2/Lab_2.py
+++ Lab_2/Lab_2.py
@@ -23,7 +23,7 @@ def on_trackbar(val):
     output = img_display.copy()
     h_img, w_img = img_display.shape[:2]
 
-    # === LANDSAT (Низька якість - Плями) ===
+    # ===LANDSAT (Низька якість - Плями)===
     if mode_current == 'low':
         blur_val = cv2.getTrackbarPos('Blur', window_name)
         thresh_val = cv2.getTrackbarPos('Threshold', window_name)
